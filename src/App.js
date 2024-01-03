f9c474b9c5a4174af9617fb8c0df5d8a282a20aa
Purva Ratanapara <115995434+purvakhant421@users.noreply.github.com>
2024-01-03 17:09:14 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Data from './components/Data';
+
+function App() {
+  return (
+    <div className="App">
+      <Data first="Jay" last="Gandhi" />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Data.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Data = (data) => {
+    return (
+        <div>
+            <h2>My fist name is {data.first}</h2>
+            <h2>My Surname is {data.last}</h2>
+        </div>
+    )
+}
+
+export default Data
